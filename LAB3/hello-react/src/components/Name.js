3c4e5d10881f46038522ebd1fdcf955f98af535e
dadwindad <vongpramate@gmail.com>
2021-11-24 11:37:18 +0700

    react

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName() {
+  return (
+    <div className="MyName">
+      <div>ดรัสวิน วงศ์ปรเมษฐ์</div>
+      <div>4430201834</div>
+    </div>
+  );
+}
+
+export default MyName;
---
+++ LAB3/readme.md
@@ -0,0 +1,7 @@
+## install
+
+> npm install
+
+## run
+
+> npm run
